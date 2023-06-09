ff8f608a60c695fd31492fb70783a4d84b21e2ea
neon14 <you@example.com>
2023-06-09 13:48:40 -0400

    new yaml file added

--- import-data.R
+++ import-data.R
@@ -3,7 +3,11 @@ library(googlesheets4)
 
 gs4_deauth()
 
-survey_data <- read_sheet("https://docs.google.com/spreadsheets/d/1Iqxq3Eb5xh_BIiLf3GOokuC9JWdOBVUycUQa-rWiWiA/edit?usp=sharing")
+event_reg <- read_sheet("https://docs.google.com/spreadsheets/d/1Iqxq3Eb5xh_BIiLf3GOokuC9JWdOBVUycUQa-rWiWiA/edit?usp=sharing")
 
-survey_data %>%
-  write_rds("survey_data.rds")
+event_reg %>%
+  write_rds("data/event_reg.rds")
+
+git2r::commit(repo = ".", message = "commit message")
+
+git2r::push("origin", "refs/head/main")
